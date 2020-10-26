16c45a95a12ac4cfa860d8dae930501001ab84b1
Mayank Ranjan <56514596+mayank722@users.noreply.github.com>
2020-10-26 19:17:00 +0530

    Create Conditional_statement.py
    Using conditional statement in python

---
+++ Conditional_statement.py
@@ -0,0 +1,13 @@
+x=5
+if x==5:
+ print('Equals 5')
+if x>4:
+ print('Greater than 4')
+if x>=5:
+ print('Greater than equal to 5')
+if x<=5:
+ print('less than equal to 5')
+if x<6:
+ print('less than 6')
+if x!=6:
+ print('not equal to 6')
